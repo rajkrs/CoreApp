7b51cf93b2e51260c6a2730c3c537340402cf201
Raj Kumar <blog.raj111@gmail.com>
2019-01-23 17:30:30 +0530

    Added value3

--- CoreApp.WebApi/Controllers/ValuesController.cs
+++ CoreApp.WebApi/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace CoreApp.WebApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
         }
 
         // GET api/values/5
